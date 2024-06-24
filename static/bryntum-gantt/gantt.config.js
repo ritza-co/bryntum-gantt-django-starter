a09274a0c7f40a55fa47d837ff2eb2574815e83a
Tatenda <31291528+rideam@users.noreply.github.com>
2024-06-24 13:53:12 +0200

    formatting

--- static/bryntum-gantt/gantt.config.js
+++ static/bryntum-gantt/gantt.config.js
@@ -1,25 +1,25 @@
-import {Gantt} from "./gantt.module.js";
+import { Gantt } from './gantt.module.js';
 
 
 const gantt = new Gantt({
-  appendTo: document.body,
-  project: {
-    taskStore: {
-      autoTree: true,
-      transformFlatData: true,
+    appendTo : document.body,
+    project  : {
+        taskStore : {
+            autoTree          : true,
+            transformFlatData : true
+        },
+        transport : {
+            load : {
+                url : '/load'
+            },
+            sync : {
+                url : '/sync'
+            }
+        },
+        autoLoad         : true,
+        autoSync         : true,
+        validateResponse : true
     },
-    transport: {
-      load: {
-        url: "/load",
-      },
-      sync: {
-        url: "/sync",
-      },
-    },
-    autoLoad: true,
-    autoSync: true,
-    validateResponse: true,
-  },
 
-  columns: [{type: "name", width: 250, text: "Tasks"}],
+    columns : [{ type : 'name', width : 250, text : 'Tasks' }]
 });
